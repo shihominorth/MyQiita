f4c2c8ee17afc1292ba5c88271912210514e7c7f
shihominorth <shihomi0104@icloud.com>
2022-10-20 13:37:05 +0900

    Initial Commit

---
+++ MyQiitaUITests/MyQiitaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyQiitaUITestsLaunchTests.swift
+//  MyQiitaUITests
+//
+//  Created by 北島　志帆美 on 2022-10-20.
+//
+
+import XCTest
+
+final class MyQiitaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
