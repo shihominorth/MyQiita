42002818fe14ff02db08496d4fceb11c9e898d7a
shihominorth <shihomi0104@icloud.com>
2022-10-24 23:57:46 +0900

    update: ArticleWebモジュール追加

---
+++ MyQiita/ArticleWebView/ArticleWebModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArticleWebModel.swift
+//  MyQiita
+//
+//  Created by 北島　志帆美 on 2022-10-24.
+//
+
+import Foundation
---
+++ MyQiita/ArticleWebView/ArticleWebView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleWebView.swift
+//  MyQiita
+//
+//  Created by 北島　志帆美 on 2022-10-24.
+//
+
+import UIKit
+
+class ArticleWebView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
